4ca64f002d7321d3c7c3af0a5d0c934b24127723
zd-prog <darya.ziziko@mail.ru>
2022-08-30 14:49:58 +0300

    The NUnit Test projects were added to the solution

---
+++ Curtains/Curtains.Application_tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Curtains.Application_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
