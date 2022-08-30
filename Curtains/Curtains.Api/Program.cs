58cc1a1b45f6c1ffc43b66f551494ab1dea6da13
zd-prog <darya.ziziko@mail.ru>
2022-08-30 17:34:50 +0300

    The Startup.cs and Program.cs were changed

--- Curtains/Curtains.Api/Program.cs
+++ Curtains/Curtains.Api/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BuildWebHost(args).Run();
+    }
+    public static IWebHost BuildWebHost(string[] args) => 
+        WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+
+}
