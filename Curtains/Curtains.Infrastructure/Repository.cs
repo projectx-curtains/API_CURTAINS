e86957cd61cecd3d04704e32722a50cbb23b796a
zd-prog <darya.ziziko@mail.ru>
2022-09-27 17:45:45 +0300

    Repository class and interface were added to the project

---
+++ Curtains/Curtains.Infrastructure/Repository.cs
@@ -0,0 +1,71 @@
+﻿using Curtains.Domain.Models;
+using Curtains.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curtains.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly CurtainsDbContext _curtainsContext;
+        private DbSet<T> entities;
+
+        public Repository(CurtainsDbContext curtainsContext)
+        {
+            _curtainsContext = curtainsContext;
+            entities = _curtainsContext.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            _curtainsContext.SaveChanges();
+        }
+        public T Get(int Id)
+        {
+            return entities.SingleOrDefault(c => c.Id == Id);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            _curtainsContext.SaveChanges();
+        }
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+        }
+        public void SaveChanges()
+        {
+            _curtainsContext.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Update(entity);
+            _curtainsContext.SaveChanges();
+        }
+    }
+}
