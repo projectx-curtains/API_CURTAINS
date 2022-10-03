a8d65387acd0b82be4b64b473c0f1984f25b4af5
zd-prog <darya.ziziko@mail.ru>
2022-10-03 18:29:22 +0300

    Regions and documentation for AccessoriesRepository were added to the project

--- Curtains/Curtains.Infrastructure/Repositories/AccessoriesRepository.cs
+++ Curtains/Curtains.Infrastructure/Repositories/AccessoriesRepository.cs
@@ -3,6 +3,7 @@ using Curtains.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using Nest;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,16 @@ using System.Threading.Tasks;
 
 namespace Curtains.Infrastructure.Repositories
 {
+    /// <summary>
+    /// Class<c> AccessoriesRepository</c> describes entity interaction model for database
+    /// </summary>
     public class AccessoriesRepository : IRepository<AccessoriesModel>
     {
+        #region FieldsRegion
         private readonly ILogger _logger;
         private readonly CurtainsDbContext _curtainsContext;
         private IQueryable<AccessoriesModel> Query => _curtainsContext.Accessories.Include(x => x.Curtains).Include(x => x.UserOrders);
+        #endregion
 
         public AccessoriesRepository(CurtainsDbContext curtainsContext, ILogger logger)
         {
@@ -24,11 +30,21 @@ namespace Curtains.Infrastructure.Repositories
             _logger = logger;
         }
 
+        #region MethodsRegion
+        /// <summary>
+        /// This method get all<c> AccessoriesModel <c> entities from database with includes tabels by entity Id
+        /// </summary>
+        /// <returns>Collection of AccessoriesModel entities in List type</return>
         public IEnumerable<AccessoriesModel> GetAll()
         {
             return _curtainsContext.Accessories.AsNoTracking().AsEnumerable();
         }
 
+        /// <summary>
+        /// This method get <c> AccessoriesModel <c> entity from database by entity Id
+        /// </summary>
+        /// <param name = "Id" > Guid types entity idetifier</param>
+        /// <returns>Accessories model</return>
         public async Task<AccessoriesModel> GetByIdAsync(int Id)
         {
             var result = await Query.AsNoTracking().SingleOrDefaultAsync(a => a.Id == Id);
@@ -42,6 +58,11 @@ namespace Curtains.Infrastructure.Repositories
             return result;
         }
 
+        /// <summary>
+        /// This method add <c> AccessoriesModel <c> entity to database
+        /// </summary>
+        /// <param name = "entity" > AccessoriesModel type</param>
+        /// <returns>EntityEntry<AccessoriesModel></return>
         public async Task<EntityEntry<AccessoriesModel>> InsertAsync(AccessoriesModel entity)
         {
             if (entity == null)
@@ -57,6 +78,10 @@ namespace Curtains.Infrastructure.Repositories
             return result;
         }
 
+        /// <summary>
+        /// This method update <c> AccessoriesModel <c> entity in database 
+        /// </summary>
+        /// <param name = "entity" > AccessoriesModel type</param>
         public async void UpdateAsync(AccessoriesModel entity)
         {
             foreach (var entry in _curtainsContext.ChangeTracker.Entries()) 
@@ -73,6 +98,10 @@ namespace Curtains.Infrastructure.Repositories
             await _curtainsContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// This method remove <c> AccessoriesModel <c> entity from database 
+        /// </summary>
+        /// <param name = "entity" > AccessoriesModel type</param>
         public async void RemoveAsync(AccessoriesModel entity)
         {
             if (entity == null)
@@ -85,9 +114,13 @@ namespace Curtains.Infrastructure.Repositories
             await _curtainsContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// This method save changes in database 
+        /// </summary>
         public async void SaveChangesAsync()
         {
             await _curtainsContext.SaveChangesAsync();
         }
+        #endregion
     }
 }
