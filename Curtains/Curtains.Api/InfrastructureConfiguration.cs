cdec896b8cf9c6af8146c96a43b7d08e444d0839
zd-prog <darya.ziziko@mail.ru>
2022-10-14 08:25:14 +0300

    Repositories were added to services

--- Curtains/Curtains.Api/InfrastructureConfiguration.cs
+++ Curtains/Curtains.Api/InfrastructureConfiguration.cs
@@ -1,9 +1,13 @@
 ﻿using Curtains.Domain.Search;
+using Curtains.Infrastructure.Correlation;
 using Curtains.Infrastructure.Database;
+using Curtains.Infrastructure.Interfaces;
+using Curtains.Infrastructure.Repositories;
 using Curtains.Infrastructure.SearchEngine;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Curtains.Api
 {
@@ -27,7 +31,30 @@ namespace Curtains.Api
 
             services
                 .AddSingleton(provider => ElasticSearchClientFactory.Create(elasticSearchOptions))
-                .AddSingleton<ICurtainsSearchRepository, CurtainsSearchRepository>();
+                .AddSingleton<ICurtainsSearchRepository, CurtainsSearchRepository>()
+                .AddSingleton<CorrelationMiddleware>();
+
+            services
+                .AddScoped<IReviewRepository, ReviewRepository>()
+                .AddScoped<IColorRepository, ColorRepository>()
+                .AddScoped<IConsistencyRepository, ConsistencyRepository>()
+                .AddScoped<ICurtainsKindRepository, CurtainsKindRepository>()
+                .AddScoped<ICurtainsTypeRepository, CurtainsTypeRepository>()
+                .AddScoped<IDecorationsRepository, DecorationsRepository>()
+                .AddScoped<IManufacturerRepository, ManufacturerRepository>()
+                .AddScoped<IMaterialRepository, MaterialRepository>()
+                .AddScoped<IAccessoriesRepository, AccessoriesRepository>()
+                .AddScoped<IFabricRepository, FabricRepository>()
+                .AddScoped<IBedspreadsRepository, BedspreadsRepository>()
+                .AddScoped<ICurtainsRepository, CurtainsRepository>()
+                .AddScoped<IPillowsRepository, PillowsRepository>()
+                .AddScoped<IProductSetRepository, ProductSetRepository>()
+                .AddScoped<IProductImageRepository, ProductImageRepository>()
+                .AddScoped<IMarketingInfoRepository, MarketingInfoRepository>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<CurtainsDbContext>>();
+            services.AddSingleton(typeof(ILogger), logger);
 
             return services;
         }
