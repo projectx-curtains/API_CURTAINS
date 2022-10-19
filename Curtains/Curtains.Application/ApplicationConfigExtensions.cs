cf34946c65e2685cd1c490a9d8ab6e12cdc494f7
egor <etolokonnikov0601@gmail.com>
2022-10-19 16:12:43 +0300

    resetup DI adding

--- Curtains/Curtains.Application/ApplicationConfigExtensions.cs
+++ Curtains/Curtains.Application/ApplicationConfigExtensions.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Curtains.Application.Interfaces;
 
 namespace Curtains.Application
 {
@@ -11,14 +12,15 @@ namespace Curtains.Application
         {
             return services
                 .AddMediatR(Assembly.GetExecutingAssembly())
-                .AddAutoMapper();
+                .AddAutoMapper()
+                .AddServices();
         }
 
         public static IServiceCollection AddAutoMapper(this IServiceCollection services)
         {
             var types = AppDomain.CurrentDomain
                 .GetAssemblies()
-                .Where(x => x.FullName.StartsWith("CurtainsService"))
+                .Where(x => x.FullName.StartsWith("Curtains.Application"))
                 .SelectMany(x => x.GetTypes()
                     .Where(type => type.IsClass
                                    && !type.IsAbstract
@@ -35,5 +37,29 @@ namespace Curtains.Application
 
             return services;
         }
-    }    
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            var types = Assembly
+
+                .GetExecutingAssembly()
+                .GetTypes();
+
+            var interfaceTypes = types
+                .Where(type => type.IsInterface
+                            && type.Namespace == typeof(IOurWorksService).Namespace)
+            .ToArray();
+
+            foreach (var interfaceType in interfaceTypes)
+            {
+                var implementation = types
+                    .FirstOrDefault(type => type.GetInterface(interfaceType.Name) == interfaceType);
+
+                services
+                    .AddScoped(interfaceType, implementation);
+            }
+
+            return services;
+        }
+    }
 }
---
+++ Curtains/Curtains.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using Curtains.Domain.Search;
+using Curtains.Infrastructure.Correlation;
+using Curtains.Infrastructure.Database;
+using Curtains.Infrastructure.SearchEngine;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Curtains.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace Curtains.Infrastructure
+{
+    public static class InfrastructureConfiguration
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            string connectionString,
+            IConfiguration configuration)
+        {
+            var elasticSearchOptions = configuration.GetSection(ElasticSearchOptions.Name).Get<ElasticSearchOptions>();
+
+            services
+                .AddDbContext<CurtainsDbContext>(opt =>
+                {
+                    opt.UseSqlServer(connectionString, opt =>
+                    {
+                        opt.MigrationsAssembly(typeof(CurtainsDbContext).Assembly.FullName);
+                    });
+                });
+
+            services
+                .AddSingleton(provider => ElasticSearchClientFactory.Create(elasticSearchOptions))
+                .AddSingleton<ICurtainsSearchRepository, CurtainsSearchRepository>()
+                .AddSingleton<CorrelationMiddleware>();
+
+            services
+                .AddRepositories();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<CurtainsDbContext>>();
+            services.AddSingleton(typeof(ILogger), logger);
+
+            return services;
+        }
+
+         private static IServiceCollection AddRepositories(this IServiceCollection services)
+         {
+            var types = Assembly
+                .GetExecutingAssembly()
+                .GetTypes();
+
+            var interfaceTypes = types
+                .Where(type => type.IsInterface
+                            && type.Namespace == typeof(ICurtainsRepository).Namespace)
+            .ToArray();
+
+        foreach (var interfaceType in interfaceTypes)
+        {
+            var implementation = types
+                .FirstOrDefault(type => type.GetInterface(interfaceType.Name) == interfaceType);
+
+            services
+                .AddScoped(interfaceType, implementation);
+        }
+
+        return services;
+         }
+    }
+}
