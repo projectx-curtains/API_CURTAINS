b3caa2885a3ba6ac1ef38ae021fd4d265cfb6ee2
zd-prog <darya.ziziko@mail.ru>
2022-10-24 21:24:35 +0300

    ProductImageController was added to the project

---
+++ Curtains/Curtains.Api/Controllers/ProductImageController.cs
@@ -0,0 +1,38 @@
+﻿using Curtains.Application.CurtainsServices.Interfaces;
+using Curtains.Application.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curtains.Api.Controllers
+{
+    /// <summary>
+    /// This controller <c> ProductImageController </c> handles requests and
+    /// changes, deletes, sends, gets feedback data.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImageController : ControllerBase
+    {
+        private readonly IProductImageService _service;
+
+        public ProductImageController(IProductImageService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// This method handles requests and get all ProductImages from database.        
+        /// </summary>
+        /// <returns> Http status code </returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductImageDTO>> GetNewItems()
+        {
+            var model = _service.GetAll().Where(n => n.MarketingInfo.IsNew);
+            return Ok(model);
+        }
+    }
+}
