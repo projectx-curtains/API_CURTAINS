9c33fcc0391f617b177b6151bd9a3a369327199a
zd-prog <darya.ziziko@mail.ru>
2022-10-25 23:24:50 +0300

    Mappings were splitted

---
+++ Curtains/Curtains.Application/AutoMapping/CurtainsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Curtains.Application.DTO;
+using Curtains.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curtains.Application.AutoMapping
+{
+    public class CurtainsProfile : Profile
+    {
+        public CurtainsProfile()
+        {
+            CreateMap<CurtainsModel, CurtainsDTO>().ReverseMap();
+        }
+    }
+}
