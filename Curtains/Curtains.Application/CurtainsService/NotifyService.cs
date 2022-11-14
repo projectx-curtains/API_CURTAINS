c2bcb24ca15d0fb0d4c6839f4b78bc476381cee5
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2022-11-14 21:11:58 +0300

    add documentation

--- Curtains/Curtains.Application/CurtainsService/NotifyService.cs
+++ Curtains/Curtains.Application/CurtainsService/NotifyService.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.Logging;
 
 namespace Curtains.Application.CurtainsService
 {
+    /// <summary>
+    /// Notifies the customer of an order
+    /// </summary>
     class NotifyService : INotifyService
     {
         private const string subject = "Вам поступил новый заказ!";
@@ -24,22 +27,34 @@ namespace Curtains.Application.CurtainsService
             _logger = logger;
         }
 
+        /// <summary>
+        /// Sends a message to the customer based on existing products
+        /// </summary>
+        /// <param name="order">Order information and item numbers</param>
+        /// <exception cref="ArgumentNullException">Throws an error if the argument is null</exception>
         public async Task NotifyAsync(OrderDTO order)
         {
             if (order is null)
             {
-                throw new ArgumentNullException("Order is null", nameof(order));
+                _logger.LogError("Order is null");
+                throw new ArgumentNullException(nameof(order), "Order is null");
             }
 
             string message = _messageBuilder.Construct(order);
             await _messageSender.SendAsync(subject, message, _config["EmailSettings:CustomerEmail"]);
         }
 
+        /// <summary>
+        /// Sends a message to the customer based on the order information
+        /// </summary>
+        /// <param name="constructorData">Information about order</param>
+        /// <exception cref="ArgumentNullException">Throws an error if the argument is null</exception>
         public async Task NotifyAsync(ConstructorDTO constructorData)
         {
             if (constructorData is null)
             {
-                throw new ArgumentNullException("Constructor data is null", nameof(constructorData));
+                _logger.LogError("Constructor data is null");
+                throw new ArgumentNullException(nameof(constructorData), "Constructor data is null");
             }
 
             string message = _messageBuilder.Construct(constructorData);
