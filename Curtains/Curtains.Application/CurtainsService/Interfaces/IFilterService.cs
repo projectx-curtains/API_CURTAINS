4577692fe069b39b39454802adf2f58adac69ad9
zd-prog <darya.ziziko@mail.ru>
2022-11-22 18:13:32 +0300

    Controller, service and interface for filters were added

---
+++ Curtains/Curtains.Application/CurtainsService/Interfaces/IFilterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curtains.Application.CurtainsService.Interfaces
+{
+    public interface IFilterService
+    {
+        
+    }
+}
