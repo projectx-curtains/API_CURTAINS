5db033df216d9a892f84c94b70e4115ba5430744
zd-prog <darya.ziziko@mail.ru>
2022-11-23 15:10:08 +0300

    Methods were added to FilterService and interface

--- Curtains/Curtains.Application/CurtainsService/FilterService.cs
+++ Curtains/Curtains.Application/CurtainsService/FilterService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using Curtains.Application.CurtainsService.Interfaces;
+using Curtains.Application.DTO.Filters;
+using Curtains.Infrastructure.Interfaces;
+using Curtains.Infrastructure.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,55 @@ using System.Threading.Tasks;
 
 namespace Curtains.Application.CurtainsService
 {
-    public class FilterService
+    /// <summary>
+    /// Class <c> FilterService </c> describes the interaction model of the data transfer object to the database.
+    /// </summary>
+    public class FilterService : IFilterService
     {
+        #region FieldsRegion
+        private readonly ILogger _logger;
+        #endregion
+
+        public FilterService(IMapper mapper, ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        #region MethodsRegion
+        Task<AccessoriesFilter> IFilterService.GetAccessoriesFilters()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<BaseFilter> IFilterService.GetAllCategoriesFilters()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<BedspreadsFilter> IFilterService.GetBedspreadsFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<CurtainsFilter> IFilterService.GetCurtainsFilters()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<FabricFilter> IFilterService.GetFabricFilters()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<LambrequinsFilter> IFilterService.GetLambrequinsFilters()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<PillowsFilter> IFilterService.GetPillowsFilter()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
--- Curtains/Curtains.Application/CurtainsService/Interfaces/IFilterService.cs
+++ Curtains/Curtains.Application/CurtainsService/Interfaces/IFilterService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Curtains.Application.DTO.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,12 @@ namespace Curtains.Application.CurtainsService.Interfaces
 {
     public interface IFilterService
     {
-        
+        public Task<CurtainsFilter> GetCurtainsFilters();
+        public Task<FabricFilter> GetFabricFilters();
+        public Task<LambrequinsFilter> GetLambrequinsFilters();
+        public Task<PillowsFilter> GetPillowsFilter();
+        public Task<BedspreadsFilter> GetBedspreadsFilter();
+        public Task<AccessoriesFilter> GetAccessoriesFilters();
+        public Task<BaseFilter> GetAllCategoriesFilters();
     }
 }
