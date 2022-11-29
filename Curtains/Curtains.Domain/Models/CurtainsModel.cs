b882b9000329744f2eb59eb1281b2ee9e597b6a2
egor <etolokonnikov0601@gmail.com>
2022-11-29 17:47:31 +0300

    fix naming

--- Curtains/Curtains.Domain/Models/CurtainsModel.cs
+++ Curtains/Curtains.Domain/Models/CurtainsModel.cs
@@ -50,7 +50,7 @@
         /// <summary>
         ///  This property represents BrasingId column.
         /// </summary>
-        public int BrasingId { get; set; }
+        public int BraсingId { get; set; }
         #endregion
 
         #region NavigationProperties
@@ -77,7 +77,7 @@
         /// <summary>
         ///  This property represents of Bracing.
         /// </summary>
-        public virtual BracingModel Brasing { get; set; }
+        public virtual BracingModel Braсing { get; set; }
         /// <summary>
         ///  This property represents of Color.
         /// </summary>
