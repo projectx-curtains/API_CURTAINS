7559b7a943bca30824b13b35f63a9bd9c65c1e21
Pilyut <pil.egor.lol@gmail.com>
2022-12-08 17:19:39 +0300

    Add filters by CurtainsType and Materials

--- Curtains/Curtains.Infrastructure/SearchEngine/Repositories/CurtainsSearchRepository.cs
+++ Curtains/Curtains.Infrastructure/SearchEngine/Repositories/CurtainsSearchRepository.cs
@@ -22,8 +22,16 @@ namespace Curtains.Infrastructure.SearchEngine
                         .Terms(t => t
                             .Field(f => f.Color)
                             .Terms(model.Filters.Colors)
+
+                            .Field(f => f.Material)
+                            .Terms(model.Filters.Materials)
+
+                            .Field(f => f.CurtainsType)
+                            .Terms(model.Filters.CurtainsTypes)
                         )
                     )
+                    .Take(model.Take)
+                    .Skip(model.Skip)
                 );
 
             if (!response.IsValid)
@@ -49,110 +57,6 @@ namespace Curtains.Infrastructure.SearchEngine
             }).ToList();
 
             return list;
-
-
         }
-            
-        /*public async Task<ISearchResponse<CurtainsProjection>> GetCurtainsByFabric(string indexName, string fabricId)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-               .Index(indexName)
-                    .Query(q => q
-                        .Match(m => m
-                            .Field(f => f.FabricId.ToString())
-                            .Query(fabricId)
-                        )
-                    )
-                );
-
-            return response;
-        }
-
-        public async Task<ISearchResponse<CurtainsProjection>> GetCurtainsByColor(string indexName, string colorId)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Index(indexName)
-                    .Query(q => q
-                        .Match(m => m
-                            .Field(f => f.ColorId.ToString())
-                            .Query(colorId)
-                        )
-                    )
-                );
-
-            return response;
-        }
-
-        public async Task<ISearchResponse<CurtainsProjection>> GetCurtainsByMaterial(string indexName, string materialId)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Index(indexName)
-                    .Query(q => q
-                        .Match(m => m
-                            .Field(f => f.MaterialId.ToString())
-                            .Query(materialId)
-                        )
-                    )
-                );
-
-            return response;
-        }
-
-        public async Task<ISearchResponse<CurtainsProjection>> GetCurtainsByCurtainsType(string indexName, string curtainsTypeId)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Index(indexName)
-                    .Query(q => q
-                        .Match(m => m
-                            .Field(f => f.CurtainsTypeId.ToString())
-                            .Query(curtainsTypeId)
-                        )
-                    )
-                );
-            return response;
-        }
-
-        public async Task<ISearchResponse<CurtainsProjection>> GetCurtainsByCurtainsKind(string indexName, string curtainsKindId)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Index(indexName)
-                    .Query(q => q
-                        .Match(m => m
-                            .Field(f => f.CurtainsKindId.ToString())
-                            .Query(curtainsKindId)
-                        )
-                    )
-                );
-            return response;
-        }
-
-        public async Task<ISearchResponse<CurtainsProjection>> GetCurtainsByPurpose(string indexName, string purpose)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Index(indexName)
-                    .Query(q => q
-                        .Term(t => t
-                            .Field(f => f.Purpose)
-                            .Value(purpose)
-                        )
-                    )
-                );
-            return response;
-        }
-
-        public async Task<ISearchResponse<CurtainsProjection>> GetProductByPriceRange(string indexName, int priceLowerLimit, int priceHigherLimit)
-        {
-            var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Index(indexName)
-                    .Query(q => q
-                        .Range(m => m
-                            .Field(f => f.Price)
-                            .GreaterThanOrEquals(priceLowerLimit)
-                            .LessThan(priceHigherLimit)
-                        )
-                    )
-                );
-            return response;
-        }*/
     }
 }
