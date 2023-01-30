63b7a19680fa4cc2232fb285f1a939096998a20d
Pilyut <pil.egor.lol@gmail.com>
2023-01-30 20:37:54 +0300

    Update Range query

--- Curtains/Curtains.Infrastructure/SearchEngine/Repositories/CurtainsSearchRepository.cs
+++ Curtains/Curtains.Infrastructure/SearchEngine/Repositories/CurtainsSearchRepository.cs
@@ -8,16 +8,18 @@ namespace Curtains.Infrastructure.SearchEngine
 {
     public class CurtainsSearchRepository : ICurtainsSearchRepository
     {
+		#region FieldsRegion
+		private readonly IElasticClient _elasticClient;
+		// private readonly ElasticSearchOptions _options;
+		#endregion
 
-        private readonly IElasticClient _elasticClient;
-       // private readonly ElasticSearchOptions _options;
-
-        public CurtainsSearchRepository(IElasticClient elasticsearchClient)
+		public CurtainsSearchRepository(IElasticClient elasticsearchClient)
         {
             _elasticClient = elasticsearchClient;
         }
 
-        public async Task<List<SearchResults<CurtainsProjection>>> GetCurtains(ElasticSearchQuery<CurtainsProjection> model)
+		#region MethodsRegion
+		public async Task<List<SearchResults<CurtainsProjection>>> GetCurtains(ElasticSearchQuery<CurtainsProjection> model)
         {
 
             var searchFields = SearchRules.CurtainsSearchFields;
@@ -43,18 +45,16 @@ namespace Curtains.Infrastructure.SearchEngine
 							mu => mu
 							.Terms(t => t.Field(f => f.Fabric).Terms(model.Filters?.Fabric)),
 							mu => mu
-							.Terms(t => t.Field(f => f.Purpose).Terms(model.Filters?.Purpose)),
-							mu => mu
-                            .Range(r => r.Field(f => f.Price).GreaterThanOrEquals(model.MinPrice).LessThanOrEquals(model.MaxPrice)
-                            .Relation(RangeRelation.Within)))))
+							.Terms(t => t.Field(f => f.Purpose).Terms(model.Filters?.Purpose)))
+
+						.Should(sh => sh
+							.Range(r => r.Field(f => f.Price).GreaterThanOrEquals(model.MinPrice).LessThanOrEquals(model.MaxPrice)
+							.Relation(RangeRelation.Within)))))
 
                 .Highlight(h => h
 					.Fields(searchFields.Select<string, Func<HighlightFieldDescriptor<CurtainsProjection>, IHighlightField>>(s =>
 							hf => hf
 								.Field(s)).ToArray())));
-
-
-			
 
 			if (!response.IsValid)
 			{
@@ -98,89 +98,6 @@ namespace Curtains.Infrastructure.SearchEngine
 			else
 				return true;
 		}
+		#endregion
 	}
-
-    /*.Query(q => q
-                    .Bool(bq => bq
-                        .Filter(fq =>
-                        {
-        QueryContainer query = null;
-
-        if (model.Filters.Colors != null)
-        {
-            query &= fq.Terms(t => t.Field(f => f.Color).Terms(model.Filters.Colors));
-        }
-
-        if (model.Filters.CurtainsTypes != null)
-        {
-            query &= fq.Terms(t => t.Field(f => f.CurtainsType).Terms(model.Filters.CurtainsTypes));
-        }
-
-        if (model.Filters.CurtainsKind != null)
-        {
-            query &= fq.Terms(t => t.Field(f => f.CurtainsKind).Terms(model.Filters.CurtainsKind));
-        }
-
-        if (model.Filters.Materials != null)
-        {
-            query &= fq.Terms(t => t.Field(f => f.Material).Terms(model.Filters.Materials));
-        }
-
-        if (model.Filters.Fabric != null)
-        {
-            query &= fq.Terms(t => t.Field(f => f.Fabric).Terms(model.Filters.Fabric));
-        }
-
-        if (model.Filters.Purpose != null)
-        {
-            query &= fq.Terms(t => t.Field(f => f.Purpose).Terms(model.Filters.Purpose));
-        }
-
-        return query;
-    })
-                    )
-                )*/
-    
 }
-
-/*var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-				.Take(model.Take)
-				.Skip(model.Skip)
-				.Query(q => q
-						.QueryString(qs => qs
-							.Query(model.Search)
-							.Fields(searchFields.Select(x => new Field(x)).ToArray())
-						)
-					)
-				.Highlight(h => h
-					.Fields(searchFields.Select<string, Func<HighlightFieldDescriptor<CurtainsProjection>, IHighlightField>>(s =>
-							hf => hf
-								.Field(s)).ToArray()))
-				);*/
-
-/*var response = await _elasticClient.SearchAsync<CurtainsProjection>(s => s
-                .Query(q => q
-                    .Bool(b => b
-                        .Must(mu => mu
-                            .MultiMatch(mm => mm
-                                .Query(model.Search)
-                                .Fields(searchFields)
-                                .Type(TextQueryType.MostFields)
-                                .PrefixLength(2)
-                                .Boost(5)
-                            )
-                        )
-                        .Must(
-                            sh => sh.Terms(t => t
-                                .Field(f => f.Purpose)
-                                .Terms(model.Filters.Purpose)
-                            ),
-                            sh => sh.Terms(t => t
-                                .Field(f => f.Color)
-                                .Terms(model.Filters.Colors)
-                            )
-                        )
-                    )
-                )
-            );*/
-
