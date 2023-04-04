03d58a2e3d2583f13752c95d27398ff10d230baa
Pilyut <pil.egor.lol@gmail.com>
2023-04-04 11:16:09 +0300

    fixed tabs in Filter.cs

--- Curtains/Curtains.Domain/Search/Filter.cs
+++ Curtains/Curtains.Domain/Search/Filter.cs
@@ -3,41 +3,41 @@ namespace Curtains.Domain.Search
 {
     public class Filter
     {
-		/// <summary>
-		/// This property is a list of curtain fabric names.
-		/// </summary>
-		public IList<string>? Fabric { get; set; }
-		/// <summary>
-		/// This property is a list of curtain type names.
-		/// </summary>
-		public IList<string>? CurtainsTypes { get; set; }
-		/// <summary>
-		/// This property is a list of curtain kind names.
-		/// </summary>
-		public IList<string>? CurtainsKind { get; set; }
-		/// <summary>
-		/// This property is a list of curtain purpose names.
-		/// </summary>
-		public IList<string>? Purpose { get; set; }
-		/// <summary>
-		/// This property is a list of curtain material names.
-		/// </summary>
-		public IList<string>? Materials { get; set; }
-		/// <summary>
-		/// This property is a list of fabric composition.
-		/// </summary>
-		public IList<string>? FabricStructure { get; set; }
-		/// <summary>
-		/// This property is a list of curtain color names.
-		/// </summary>
-		public IList<string>? Colors { get; set; }
-		/// <summary>
-		/// This property is a list of curtain design names.
-		/// </summary>
-		public IList<string>? Design { get; set; }
-		/// <summary>
-		/// This property is a list of curtain bracing names.
-		/// </summary>
-		public IList<string>? Bracing { get; set; }
-	}
+        /// <summary>
+        /// This property is a list of curtain fabric names.
+        /// </summary>
+        public IList<string>? Fabric { get; set; }
+        /// <summary>
+        /// This property is a list of curtain type names.
+        /// </summary>
+        public IList<string>? CurtainsTypes { get; set; }
+        /// <summary>
+        /// This property is a list of curtain kind names.
+        /// </summary>
+        public IList<string>? CurtainsKind { get; set; }
+        /// <summary>
+        /// This property is a list of curtain purpose names.
+        /// </summary>
+        public IList<string>? Purpose { get; set; }
+        /// <summary>
+        /// This property is a list of curtain material names.
+        /// </summary>
+        public IList<string>? Materials { get; set; }
+        /// <summary>
+        /// This property is a list of fabric composition.
+        /// </summary>
+        public IList<string>? FabricStructure { get; set; }
+        /// <summary>
+        /// This property is a list of curtain color names.
+        /// </summary>
+        public IList<string>? Colors { get; set; }
+        /// <summary>
+        /// This property is a list of curtain design names.
+        /// </summary>
+        public IList<string>? Design { get; set; }
+        /// <summary>
+        /// This property is a list of curtain bracing names.
+        /// </summary>
+        public IList<string>? Bracing { get; set; }
+    }
 }
