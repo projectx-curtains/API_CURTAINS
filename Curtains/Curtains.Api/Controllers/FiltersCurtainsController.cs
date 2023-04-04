4825c36aa3fad085e3449dcae861f51bcc808d1b
Pilyut <pil.egor.lol@gmail.com>
2023-04-04 11:02:14 +0300

    Fixed tabs

--- Curtains/Curtains.Api/Controllers/FiltersCurtainsController.cs
+++ Curtains/Curtains.Api/Controllers/FiltersCurtainsController.cs
@@ -6,12 +6,12 @@ using Curtains.Infrastructure.SearchQueries;
 
 namespace Curtains.Api.Controllers
 {
-	/// <summary>
-	/// This controller <c> FiltersCurtainsController </c> for indexes 
-	/// and searchind data from ElastiSearch 
-	/// </summary>
-	[Route("api/[controller]")]
-	[ApiController]
+    /// <summary>
+    /// This controller <c> FiltersCurtainsController </c> for indexes 
+    /// and searchind data from ElastiSearch 
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
     public class FiltersCurtainsController : ControllerBase
     {
         private readonly ICurtainSearchService _searchService;
@@ -21,36 +21,36 @@ namespace Curtains.Api.Controllers
             _searchService = searchService;
         }
 
-		/// <summary>
-		/// This method handles requests and indexes data to ElastiSearch from database
-		/// </summary>
-		/// <returns></returns>
-		[Route("Index")]
-		[HttpGet]
+        /// <summary>
+        /// This method handles requests and indexes data to ElastiSearch from database
+        /// </summary>
+        /// <returns></returns>
+        [Route("Index")]
+        [HttpGet]
         public async Task<ActionResult> AddAllCurtains()
         {
             await _searchService.IndexesCurtains();
             return Ok();
         }
 
-		/// <summary>
-		/// This method handles requests and delete curtain from ElasticSearch by entity Id.
-		/// </summary>
-		/// <param name="id"> Id curtain identifier </param>
-		/// <returns></returns>
-		[Route("Delete")]
-		[HttpGet]
+        /// <summary>
+        /// This method handles requests and delete curtain from ElasticSearch by entity Id.
+        /// </summary>
+        /// <param name="id"> Id curtain identifier </param>
+        /// <returns></returns>
+        [Route("Delete")]
+        [HttpGet]
         public async Task<ActionResult> DeleteCurtain(string id)
         {
             await _searchService.DeleteCurtain(id);
             return Ok();
         }
 
-		/// <summary>
-		/// This method handles requests and applies search filters for curtains from ElasticSearch
-		/// </summary>
-		/// <param name="request"> Seatch query </param>
-		/// <returns> Http status code 200 </returns>
+        /// <summary>
+        /// This method handles requests and applies search filters for curtains from ElasticSearch
+        /// </summary>
+        /// <param name="request"> Seatch query </param>
+        /// <returns> Http status code 200 </returns>
 		[Route("CurtainsSearch")]
 		[HttpPost]
         public async Task<ActionResult<List<SearchResults<CurtainsProjection>>>> CurtainSearch([FromQuery] ElasticSearchQuery<CurtainSearchDTO> request)
--- Curtains/Curtains.Domain/Projections/CurtainsProjection.cs
+++ Curtains/Curtains.Domain/Projections/CurtainsProjection.cs
@@ -2,65 +2,65 @@
 {
     public class CurtainsProjection
     {
-		/// <summary>
-		/// This property is an identifier for data tranfer object.
-		/// </summary>
-		public int Id { get; set; }
-		/// <summary>
-		/// This property represents the price of curtains.
-		/// </summary>
-		public decimal Price { get; set; }
-		/// <summary>
-		/// This property is the name of fabric for curtains.
-		/// </summary>
-		public string Fabric { get; set; }
-		/// <summary>
-		/// This property is the name of curtains type.
-		/// </summary>
-		public string CurtainsType { get; set; }
-		/// <summary>
-		/// This property is the name of curtains kind.
-		/// </summary>
-		public string CurtainsKind { get; set; }
-		/// <summary>
-		/// This property is the name of purpose for curtains.
-		/// </summary>
-		public string Purpose { get; set; }
-		/// <summary>
-		/// This property is the name of material for curtains.
-		/// </summary>
-		public string Material { get; set; }
-		/// <summary>
-		/// This property represents the composition of curtains.
-		/// </summary>
-		public string FabricStructure { get; set; }
-		/// <summary>
-		/// This property represents whether the curtains are blackout or not.
-		/// </summary>
-		public bool Density { get; set; }
-		/// <summary>
-		/// This property represents situation, if curtains are sun-protected or not.
-		/// </summary>
-		public bool SunProtection { get; set; }
-		/// <summary>
-		/// This property is the color name for curtains.
-		/// </summary>
-		public string Color { get; set; }
-		/// <summary>
-		/// This property represents height of curtains.
-		/// </summary>
-		public int Height { get; set; }
-		/// <summary>
-		/// This property represents width of curtains.
-		/// </summary>
-		public int Width { get; set; }
-		/// <summary>
-		/// This property is the name of design for curtains.
-		/// </summary>
-		public string Desing { get; set; }
-		/// <summary>
-		/// This property is the name of bracing for curtains.
-		/// </summary>
-		public string Bracing { get; set; }
-	}
+        /// <summary>
+        /// This property is an identifier for data tranfer object.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// This property represents the price of curtains.
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// This property is the name of fabric for curtains.
+        /// </summary>
+        public string Fabric { get; set; }
+        /// <summary>
+        /// This property is the name of curtains type.
+        /// </summary>
+        public string CurtainsType { get; set; }
+        /// <summary>
+        /// This property is the name of curtains kind.
+        /// </summary>
+        public string CurtainsKind { get; set; }
+        /// <summary>
+        /// This property is the name of purpose for curtains.
+        /// </summary>
+        public string Purpose { get; set; }
+        /// <summary>
+        /// This property is the name of material for curtains.
+        /// </summary>
+        public string Material { get; set; }
+        /// <summary>
+        /// This property represents the composition of curtains.
+        /// </summary>
+        public string FabricStructure { get; set; }
+        /// <summary>
+        /// This property represents whether the curtains are blackout or not.
+        /// </summary>
+        public bool Density { get; set; }
+        /// <summary>
+        /// This property represents situation, if curtains are sun-protected or not.
+        /// </summary>
+        public bool SunProtection { get; set; }
+        /// <summary>
+        /// This property is the color name for curtains.
+        /// </summary>
+        public string Color { get; set; }
+        /// <summary>
+        /// This property represents height of curtains.
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// This property represents width of curtains.
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// This property is the name of design for curtains.
+        /// </summary>
+        public string Desing { get; set; }
+        /// <summary>
+        /// This property is the name of bracing for curtains.
+        /// </summary>
+        public string Bracing { get; set; }
+    }
 }
